1f2881df54941f43cefef7eaf4f65c8c0a738533
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-01-23 11:18:44 -0300

    erro cors

--- src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
+++ src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*").allowedOrigins(corsOrigins);
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE", "PUT").allowedOrigins(corsOrigins);
     }
 }
